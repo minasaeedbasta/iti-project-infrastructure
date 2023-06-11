a4668cbfa856e2a4fecc6f78acba3e2f0f172703
minasaiedbasta <mina.saied.basta@gmail.com>
2023-06-11 16:15:03 +0300

    update terraform cluster setting

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,25 +1,50 @@
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
+# GKE service account
+resource "google_service_account" "custom_service_account" {
+  account_id   = "jenkins-deployer"
+  display_name = "GKE Custom Service Account"
 }
 
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
+#GKE service account key
+# resource "google_service_account_key" "mykey" {
+#   service_account_id = google_service_account.custom_service_account.name
+#   public_key_type    = "TYPE_X509_PEM_FILE"
+# }
 
-variable "gke_num_nodes" {
-  default     = 2
-  description = "number of gke nodes"
-}
+# module "my-app-workload-identity" {
+#   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   use_existing_gcp_sa = true
+#   name                = google_service_account.custom_service_account.account_id
+#   project_id          = var.project_id
+
+#   # wait for the custom GSA to be created to force module data source read during apply
+#   # https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/issues/1059
+#   depends_on = [google_service_account.custom_service_account]
+# }
+
 
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
-  location = var.region
+  location = "us-central1-a"#var.region
   
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  # Enable Workload Identity
+  workload_identity_config {
+    workload_pool  = "${var.project_id}.svc.id.goog"
+  }
+  
+  
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+    disk_size_gb = 30
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+  }
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
@@ -28,45 +53,32 @@ resource "google_container_cluster" "primary" {
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = google_container_cluster.primary.name
-  location   = var.region
+  location   = "us-central1-a"#var.region
   cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
-
+  node_count = 1
+  
   node_config {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.custom_service_account.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
 
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+
     labels = {
       env = var.project_id
     }
 
-    # preemptible  = true
+    preemptible  = true
     machine_type = "e2-small"
+    disk_size_gb = 30
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
     }
   }
 }
-
-
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
-
-# provider "kubernetes" {
-#   load_config_file = "false"
-
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
-
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -24,3 +24,17 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
+
+# Firewall Rule
+resource "google_compute_firewall" "rules" {
+  project     = var.project_id
+  name        = "jenkins-rule"
+  network     = google_compute_network.vpc.name
+  description = "Allow access to jenkins node port service"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["30009"]
+  }
+  source_ranges = [ "0.0.0.0/0" ]
+}
