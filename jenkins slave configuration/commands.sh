445af6e83699098fbf55f1724c38795851892740
minasaiedbasta <mina.saied.basta@gmail.com>
2023-06-13 19:14:23 +0300

    upload screenshots

--- jenkins slave configuration/commands.sh
+++ jenkins slave configuration/commands.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
-# give read+write permission for all
-chmod 666 /var/run/docker.sock
-
 # start ssh service
 service ssh start
 
-# set jenkins user password (123456)
-passwd jenkins
+# install gcloud
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl sudo
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-cli
+
+# install gcloud auth plugin
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
